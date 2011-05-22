9770e8fea047eed3055c9e9abc4a38250764cc41
Dave DeCarlo <dec114@gmail.com>
2011-05-22 17:53:13 -0400

    added production stat

--- seed.rb
+++ seed.rb
@@ -1,17 +1,17 @@
 module Seed
-  
-  =begin
-  h     hits
-  ab    at bats
-  bb    base on balls (walks)
-  hbp   times hit by a pitch
-  sf    sacrifice flies
-  s     singles
-  d     doubles
-  t     triples
-  hr    home runs
-  tb    total bases
-  =end
+  extend self
+=begin
+        h     hits
+        ab    at bats
+        bb    base on balls (walks)
+        hbp   times hit by a pitch
+        sf    sacrifice flies
+        s     singles
+        d     doubles
+        t     triples
+        hr    home runs
+        tb    total bases
+=end
   
   def batting_avg(h=0, ab=0)
     h.to_f/ab.to_f
@@ -27,6 +27,10 @@ module Seed
   
   def runs_created(h=0, bb=0, s=0, d=0, t=0, hr=0, ab=0)
     ((h+bb)*total_bases(s,d,t,hr)).to_f/(ab+bb).to_f
+  end
+  
+  def production(s=0, d=0, t=0, hr=0, ab=0, h=0, bb=0, hbp=0, sf=0)
+    slugging_per(s, d, t, hr, ab)+on_base_per(h, bb, hbp, ab, sf)
   end
   
   def total_bases(s=0, d=0, t=0, hr=0)
