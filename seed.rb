36127237d46308712342a7853688d9f711d7c0a9
Dave DeCarlo <dec114@gmail.com>
2011-05-22 15:39:13 -0400

    a few more stats

--- seed.rb
+++ seed.rb
@@ -1,6 +1,37 @@
 module Seed
-  def on_base(h=0, bb=0, hbp=0, ab=0, sf=0)
-    obp = (h+bb+hbp).to_f/(ab+bb+hbp+sf).to_f
+  
+  =begin
+  h     hits
+  ab    at bats
+  bb    base on balls (walks)
+  hbp   times hit by a pitch
+  sf    sacrifice flies
+  s     singles
+  d     doubles
+  t     triples
+  hr    home runs
+  tb    total bases
+  =end
+  
+  def batting_avg(h=0, ab=0)
+    h.to_f/ab.to_f
   end
+  
+  def on_base_per(h=0, bb=0, hbp=0, ab=0, sf=0)
+    (h+bb+hbp).to_f/(ab+bb+hbp+sf).to_f
+  end
+  
+  def slugging_per(s=0, d=0, t=0, hr=0, ab=0)
+    total_bases(s,d,t,hr).to_f/ab.to_f
+  end
+  
+  def runs_created(h=0, bb=0, s=0, d=0, t=0, hr=0, ab=0)
+    ((h+bb)*total_bases(s,d,t,hr)).to_f/(ab+bb).to_f
+  end
+  
+  def total_bases(s=0, d=0, t=0, hr=0)
+    s+(2*d)+(3*t)+(4*hr)
+  end
+  
 end
     
