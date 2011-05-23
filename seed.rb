c5c908e6da321d311fb486b1b1e7aff7b5692642
Dave DeCarlo <dec114@gmail.com>
2011-05-22 20:44:42 -0400

    some pitching stats too

--- seed.rb
+++ seed.rb
@@ -1,6 +1,9 @@
 module Seed
   extend self
+  
 =begin
+        BATTING
+
         h     hits
         ab    at bats
         bb    base on balls (walks)
@@ -11,6 +14,13 @@ module Seed
         t     triples
         hr    home runs
         tb    total bases
+        
+        PITCHING
+        
+        era   earned runs allowed
+        ip    innings pitched - formats can be x, x.1 or x.333, x.2 or x.666
+        bb    walks allowed
+        h     hits allowd
 =end
   
   def batting_avg(h=0, ab=0)
@@ -29,7 +39,7 @@ module Seed
     ((h+bb)*total_bases(s,d,t,hr)).to_f/(ab+bb).to_f
   end
   
-  def production(s=0, d=0, t=0, hr=0, ab=0, h=0, bb=0, hbp=0, sf=0)
+  def on_base_plus_slugging(s=0, d=0, t=0, hr=0, ab=0, h=0, bb=0, hbp=0, sf=0)
     slugging_per(s, d, t, hr, ab)+on_base_per(h, bb, hbp, ab, sf)
   end
   
@@ -37,5 +47,22 @@ module Seed
     s+(2*d)+(3*t)+(4*hr)
   end
   
+  def earned_run_avg(era=0, ip=0)
+    9*(era.to_f/format_ip(ip))
+  end
+  
+  def whip(bb=0, h=0, ip=0)
+    (bb+h).to_f/format_ip(ip)
+  end
+  
+  def format_ip(ip)
+    if ip.to_s[-2,2] == '.1'
+      ip+0.233
+    elsif ip.to_s[-2,2] == '.2'
+      ip+0.466
+    else
+      ip.to_f
+    end
+  end
 end
     
